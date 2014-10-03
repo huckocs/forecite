3bd4e34fa87f513e8c65121b3b8bbb7dfe2fc49d
robL <rob.lloyd@springer.com>
2014-10-03 09:48:02 +0100

    RobL: push to forecite

--- dist/js/forecite.js
+++ dist/js/forecite.js
@@ -1,9 +1,57 @@
-var elementPosition = $('.suggestions').offset();
-
+var suggestionBox = $('.suggestion')
+var sidebar = $('.suggestionSidebar');
+var suggestionPage = "/suggestions"
+var elementPosition = sidebar.offset();
+var citation = $('.citation');
+var numberRecs = $('.numberRecs');
+var citationHolder = $('.citation_holder');
+var i=$('.text_analysis').attr('example-no');
 $(window).scroll(function(){
   if($(window).scrollTop() > elementPosition.top){
-    $('.suggestions').addClass('is-sticky');
+    sidebar.addClass('is-sticky');
   } else {
-    $('.suggestions').removeClass('is-sticky');
+    sidebar.removeClass('is-sticky');
   }
+});
+citation.click(
+  function(){
+    var id = $(this).attr('id');
+    suggestionBox.fadeOut(function(){
+      suggestionBox.load(
+        suggestionPage+i+".html" + " " +id, function(){
+          var _this = $(this);
+          var numberSuggestions= _this.find('.citing_article').length;
+          _this.fadeIn();
+          console.log(numberSuggestions);
+          if(numberSuggestions === 1){
+            numberRecs.html(numberSuggestions + ' Recommendation');
+          }
+          else if (numberSuggestions > 1) {
+            numberRecs.html(numberSuggestions + ' Recommendations');
+          }
+          numberRecs.fadeIn();
+        })
+    })
+  }
+)
+
+suggestionBox.on('click', '.citing_article',
+  function(){
+    var _this = $(this);
+    var id = _this.parent().attr('id');
+    var citationPos = $('#'+id);
+    var citation = _this.find('.citing_text');
+    $('.shown_suggestion').removeClass('shown_suggestion');
+    _this.addClass('shown_suggestion');
+    citationHolder.empty();
+    citationHolder.append(citation.clone());
+    citationHolder.insertAfter(".citation[id=#"+ id +"]");
+    citationHolder.show();
+    citation.fadeIn();
+  }
+);
+
+citation.each(function(){
+  var _this = $(this);
+  var id = $(this).attr('id');
 });
